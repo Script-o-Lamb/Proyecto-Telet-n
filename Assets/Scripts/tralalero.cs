7303d333ccb36945f5badf270015c334d576d326
LambCode <aramirtamayo@gmail.com>
2025-04-02 20:35:17 -0300

    tralalero tralalá

--- Assets/Scripts/tralalero.cs
+++ Assets/Scripts/tralalero.cs
@@ -5,7 +5,7 @@ public class tralalero : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Tralalero tralalá");
     }
 
     // Update is called once per frame
