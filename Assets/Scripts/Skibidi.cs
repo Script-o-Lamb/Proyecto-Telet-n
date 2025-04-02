5dc29f0bd0e52426d0e327dd1483297a06426a04
LambCode <aramirtamayo@gmail.com>
2025-04-02 20:18:59 -0300

    Primer script
    Añadí el script skibidi para hacer un ejemplon't

---
+++ Assets/Scripts/Skibidi.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Skibidi : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hola Teleton't");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
